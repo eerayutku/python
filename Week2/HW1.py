64734fbdcad1571a08cbd1932ae308409d3475b1
eerayutku <36761438+eerayutku@users.noreply.github.com>
2018-03-09 13:34:32 +0300

    Week2

--- Week2/HW1.py
+++ Week2/HW1.py
@@ -1,13 +1,15 @@
 import random
 sayi = 0
+ilktoplam = 0
 toplam = 0
 a = [None] * 100
 for i in range(1, 100):
     a[i] = i
+    ilktoplam = ilktoplam + i
 a[0] = random.randint(1,99)
 random.shuffle(a)
 print(a)
 for i in a:
     toplam = toplam + i
-sayi = toplam - 4950
+sayi = toplam - ilktoplam
 print("random:",sayi)
